2b6aba13a89775ddd466580d6ffd286210394c43
RonArel <RonusTheBoss123@gmail.com>
2017-08-07 18:29:22 -0700

    Upload All the Files

---
+++ TheRealApp/app/src/main/java/com/tag/instagramdemo/example/ApplicationData.java
@@ -0,0 +1,9 @@
+package com.tag.instagramdemo.example;
+
+//username = userInfoHashmap.get(InstagramApp.TAG_USERNAME)
+public class ApplicationData {
+	public static final String CLIENT_ID = "1e27632cadf443a494c8fd827682d60b";
+	public static final String CLIENT_SECRET = "24d71635a4a4414cab9a87f5877531f1";
+	public static final String CALLBACK_URL = "https://dating.netsgets.tk";
+}
+
